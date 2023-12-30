7c8b9fbe1e91549f62d2c4591ab9368ff3be3768
John Patrick Gonzales <Johnpatrickgonzales2608@gmail.com>
2023-12-31 01:28:34 +0800

    Delete twttr.py

--- twttr.py
+++
@@ -1,8 +0,0 @@
-text = input("Input: ")
-consonants = ['a', 'e', 'i', 'o', 'u']
-for c in text:
-    if c.lower() in consonants:
-        text = text.replace(c, "")
-       
-
-print("Output:", text)
